180299be081c532550b4746e468c809b9c4544d4
yufengqinli <62143471+yufengqinli@users.noreply.github.com>
2022-09-19 22:40:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -29,8 +29,8 @@ sed -i "s/OpenWrt /${Yufeng} Compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt 
 sed -i 's/luci-theme-bootstrap/luci-theme-bootstrap/g' feeds/luci/collections/luci/Makefile                    
 
 # Uncomment a feed source
-sed -i 's/^#\(.*small8\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
